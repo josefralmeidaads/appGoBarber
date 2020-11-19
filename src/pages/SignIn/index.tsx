18551eb74fb6d4cdee1fcaf6b295af534294459f
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-11-19 17:44:48 -0300

    Adicionado as telas principais a aplicação mobile

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,65 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button'
 
 import logoImg from '../../assets/logo.png';
 
+import { Container, Title, ForgotPassword, ForgtoPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
+
 const SignIn: React.FC = () => {
+
+  const navigation = useNavigation();
+
+  const handleToSignUp = () => {
+    navigation.navigate("SignUp");
+  }
+
   return (
-      <Container>
-        <Image source={logoImg} />
-      </Container>
+  <>
+    <KeyboardAvoidingView 
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="Email"/>
+          <Input name="password" icon="lock" placeholder="Senha"/>
+
+          <Button onPress={() => {}}>
+            Entrar
+          </Button>
+
+          <ForgotPassword onPress={() => {}}>
+            <ForgtoPasswordText>
+              Esqueci minha senha
+            </ForgtoPasswordText>
+          </ForgotPassword>
+
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    <CreateAccountButton onPress={handleToSignUp}>
+      <Icon name="log-in" size={20} color="#FF9000"/>
+      <CreateAccountButtonText>
+        Criar uma conta
+      </CreateAccountButtonText>
+    </CreateAccountButton>
+    
+  </>
   );
 }
 
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button'
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignInButton, BackToSignInButtonText } from './styles';
 
 const SignUp: React.FC = () => {
+
+  const navigation = useNavigation();
+
+  const handleBackToSignIn = () => {
+    navigation.goBack();
+  }
+
   return (
-    <Container>
-    </Container>  
+    <>
+    <KeyboardAvoidingView 
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome"/>
+          <Input name="email" icon="mail" placeholder="Email"/>
+          <Input name="password" icon="lock" placeholder="Senha"/>
+
+          <Button onPress={() => {}}>
+            Entrar
+          </Button>
+
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    <BackToSignInButton onPress={handleBackToSignIn}>
+      <Icon name="arrow-left" size={20} color="#FFF"/>
+      <BackToSignInButtonText>
+        Voltar para logon
+      </BackToSignInButtonText>
+    </BackToSignInButton>
+    
+  </> 
   );
 }
 
